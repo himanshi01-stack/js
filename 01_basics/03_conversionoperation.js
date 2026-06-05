ddaaffdf2966c0a06f36e2fbac3b362bfdd0db42
himanshi01-stack <himanshisinghujn@gmail.com>
2026-06-05 16:53:34 +0000

    conversion of datatypes

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -26,3 +26,44 @@ let someNumber = "88"
 let stringNumber = String(someNumber) //similarly used to convert to string
 console.log (stringNumber);
 console.log (typeof stringNumber);
+
+/* OPERATIONS */
+
+let value = 3
+let negaValue = -value // used to convert to negative value
+console.log (negaValue);
+
+//console.log(2+2), (2-2), (2*2), (2**3), (2/3), (2%3)
+
+let str1 = "hello"
+let str2 = " himanshi"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log ("1" + 2); //o/p => 12 (string)
+console.log (1 + "2"); //o/p => 12 (string)
+console.log ("1" + 2 + 2); //o/p => 122
+console.log (1 + 2 + "2"); //o/p => 32
+
+console.log (( 3 + 4) * 5 % 3); // always use parenthesis to increase readability
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); //value increment hokar print hogi
+
+let x = 3;
+const y = x++; //postfix increment
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a; //prefix increment
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"

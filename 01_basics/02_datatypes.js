6266f75738194ac007cca07deaee10699d826dd0
himanshi01-stack <himanshisinghujn@gmail.com>
2026-06-05 06:25:42 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3+3)// we are using nodejs and not browser, isliye yeh function run karne par error aayega
+
+console.log (3 + 3) //code readability should be high
+console.log("Himanshi") //isliye dono codes ko alag alag line me likhenge
+
+let name = "Himanshi"
+let age = 21
+let isLoggedIn = false
+let State = null
+let state; //undefined
+
+//number => range- 2 to power 53
+//bigint => values greater than number
+//string => "",''
+//boolean => true/false
+//null => standalone value, representation of an empty value
+//undefined => variable declare kar diya hai but koi bhi value nhi di hai
+//symbol => uniqueness // all these are primitive datatypes
+
+//object
+
+console.log (typeof undefined); //o/p:- undefined, khud ek datatype hai
+console.log (typeof null); //o/p:- object, null object ke andar aata hai
+
+//typeof => datatypes batata hai
